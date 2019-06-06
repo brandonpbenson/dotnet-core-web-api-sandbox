78f6331d16e5e7ffb83b40b6e6e4465399983653
ralphwcoggins <ralphwcoggins@gmail.com>
2019-06-06 14:44:02 -0700

    refactor(Startup): wire up `UserManager`, `UnitOfWork`

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,8 @@ namespace Sandbox
             var connString = $"Server={hostname};User ID=sa;Password={password};";
 
             services.AddDbContext<UserContext>(opts => opts.UseSqlServer(connString));
-            services.AddScoped<IDataRepository<User>, UserManager>();
+            services.AddScoped<IUserRepository, UserManager>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
