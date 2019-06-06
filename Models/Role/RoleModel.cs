360720bcaabcb062dedd8e173ce3cb519d449e49
ralphwcoggins <ralphwcoggins@gmail.com>
2019-06-06 16:48:05 -0700

    feat(Models): add `RoleModel` domain model

---
+++ Models/Role/RoleModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+ 
+namespace Sandbox.Models.Role
+{
+    public class RoleModel
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
