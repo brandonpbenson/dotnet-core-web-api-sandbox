f879ec10241cbeae878183dbf51e02421129eabf
ralphwcoggins <ralphwcoggins@gmail.com>
2019-06-07 14:39:20 -0700

    chore(Migrations): update migrations

---
+++ Migrations/20190607210648_UpdateMigrationsToMatchNewEntityModels.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sandbox.Migrations
+{
+    public partial class UpdateMigrationsToMatchNewEntityModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "UserRoleId",
+                table: "UserRoles",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleId",
+                table: "Roles",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserRoles",
+                newName: "UserRoleId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Roles",
+                newName: "RoleId");
+        }
+    }
+}
