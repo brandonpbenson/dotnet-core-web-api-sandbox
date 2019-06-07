ab624c983bec61b1ff20123de182eb7fe2844308
ralphwcoggins <ralphwcoggins@gmail.com>
2019-06-07 13:22:07 -0700

    chore(Migrations): update migration

---
+++ Migrations/20190606220921_AddNewSeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sandbox.Migrations
+{
+    public partial class AddNewSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "UserRoleId", "RoleId", "UserId" },
+                values: new object[] { 1, 2, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Email", "FirstName", "LastName", "PasswordHash" },
+                values: new object[] { 2, "admin@admin.com", "Corey", "Shuman", null });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "UserRoleId", "RoleId", "UserId" },
+                values: new object[] { 2, 1, 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumn: "UserRoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumn: "UserRoleId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+        }
+    }
+}
